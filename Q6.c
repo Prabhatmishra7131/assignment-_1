67620a791b0995c73ecfa4d4ea33e642cc0ea709
prabhat mishra <115269832+Prabhatmishra7131@users.noreply.github.com>
2022-10-25 23:45:58 +0530

    Update Q6.c

--- Q6.c
+++ Q6.c
@@ -1,9 +1,6 @@
 #include <stdio.h>
 int main()
 {
-    char a, b, c, d, e, f,g;
-    printf("Enter the name");
-    scanf("%c%c%c%c%c%c%c", &a, &b, &c, &d, &e, &f,&g);
-    printf("Hello , %c%c%c%c%c%c%c \n", a, b, c, d, e, f, g);
+   printf("\"Hello, Amit Kumar\"");
     return 0;
 }
