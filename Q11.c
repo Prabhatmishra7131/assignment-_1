dec8fe0145f2e4c0e6c84234f0628407206f50ba
prabhat mishra <115269832+Prabhatmishra7131@users.noreply.github.com>
2022-10-25 23:52:24 +0530

    Update Q11.c

--- Q11.c
+++ Q11.c
@@ -3,7 +3,7 @@ int main()
 {
     int h,m;
     printf("Enter the time in the HH:MM format\n");
-    scanf("%d %d",&h,&m);
+    scanf("%d:%d",&h,&m);
     printf("%d Hour and %d Minute\n",h,m);
     return 0;
 }
